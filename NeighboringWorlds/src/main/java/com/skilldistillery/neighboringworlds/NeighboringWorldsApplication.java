f8ca62641780612f67965fc0f8961d3b9b085e3a
Brian Teets <bwt17@msn.com>
2022-06-22 10:03:37 -0600

    created boot project, updated application properties and project dependencies

---
+++ NeighboringWorlds/src/main/java/com/skilldistillery/neighboringworlds/NeighboringWorldsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.neighboringworlds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighboringWorldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighboringWorldsApplication.class, args);
+	}
+
+}
---
+++ NeighboringWorlds/src/test/java/com/skilldistillery/neighboringworlds/NeighboringWorldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.neighboringworlds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighboringWorldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
