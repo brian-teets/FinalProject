e991baddcd8ea2755db6ae4e059f63865e0dcbf2
paul-chaffin <pechaffin@pm.me>
2022-06-24 17:23:06 -0400

    initial commit

---
+++ NeighboringWorlds/src/main/java/com/skilldistillery/neighboringworlds/services/ReviewServiceImpl.java
@@ -0,0 +1,38 @@
+package com.skilldistillery.neighboringworlds.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.neighboringworlds.entities.Review;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+	@Override
+	public List<Review> index(int cid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Review show(int rid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Review create(Review rev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Review delete(int rid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
