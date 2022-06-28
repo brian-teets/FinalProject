afe19f2d3184ff61ba86ef08312287449d93a8fe
Erich Schindler <erschindler@gmail.com>
2022-06-28 17:07:26 -0400

    app.module.ts change

--- ngNeighboringWorlds/src/app/app.module.ts
+++ ngNeighboringWorlds/src/app/app.module.ts
@@ -1,29 +1,26 @@
-import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DatePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { NewEventComponent } from './components/new-event/new-event.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthService } from './services/auth.service';
-import { ProfileComponent } from './components/profile/profile.component';
-import { NewEventComponent } from './components/new-event/new-event.component';
-import { FooterComponent } from './components/footer/footer.component';
-
 import { ReviewComponent } from './components/review/review.component';
-
 import { DiscussionBoardComponent } from './components/discussion-board/discussion-board.component';
-
 
 @NgModule({
   declarations: [
@@ -39,22 +36,17 @@ import { DiscussionBoardComponent } from './components/discussion-board/discussi
     ProfileComponent,
     NewEventComponent,
     FooterComponent,
-
-    ReviewComponent
-
-    DiscussionBoardComponent,
-
-
-
+    ReviewComponent,
+    DiscussionBoardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [AuthService, DatePipe],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
